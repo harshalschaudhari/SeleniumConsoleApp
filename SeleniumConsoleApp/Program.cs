eeb21d0fc663a06186913a6c5a09ab3fe3be25a7
Harshal Chaudhari <harshalschaudhari@gmail.com>
2024-01-03 23:05:57 +0530

    Learn Selenium - First commit

---
+++ SeleniumConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+string symbol = "SBIN";
+string exchange = "nse";
+
+string pageSource = string.Empty;
+ChromeOptions options = new ChromeOptions();
+var service = ChromeDriverService.CreateDefaultService();
+//service.HideCommandPromptWindow = true;
+//service.SuppressInitialDiagnosticInformation = true;
+using (IWebDriver driver = new ChromeDriver(service, options))
+{
+    string url = string.Format("https://www.google.com/search?q={0}+{1}+stock+price+live", symbol.Replace("&", "%26"), exchange);
+    driver.Navigate().GoToUrl(url);
+    pageSource = driver.PageSource;
+    driver.Close();
+}
+//Console.WriteLine(pageSource);
